15343607c563b1cdf65dd845a20772fffdb25fdf
Jason <jason@Jasons-MacBook-Pro.local>
2022-10-09 19:12:58 -0600

    update website for the meta data

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-portfolio-gilt.vercel.app/",
+  site: "https://www.jasonsy.dev",
   integrations: [tailwind()],
   vite: {
     ssr: {
